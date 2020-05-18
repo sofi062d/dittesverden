c3d8215912d3e23624e080ce2ba63b3234bb59f5
mariaejdesgaard <mariaejdesgaard@gmail.com>
2020-05-18 16:12:32 +0200

    Version 4 - 18052020

--- karrusel.js
+++ karrusel.js
@@ -2,16 +2,16 @@
     // setup your carousels as you normally would using JS
     // or via data attributes according to the documentation
     // https://getbootstrap.com/javascript/#carousel
-    $("#carousel123").carousel({
-        interval: 2000
+    $('#carousel123').carousel({
+        interval: 3000
     });
-    $("#carouselABC").carousel({
-        interval: 3600
+    $('#carouselABC').carousel({
+        interval: 6600
     });
-})();
+}());
 
 (function () {
-    $(".carousel-showmanymoveone .item").each(function () {
+    $('.carousel-showmanymoveone .item').each(function () {
         var itemToClone = $(this);
 
         for (var i = 1; i < 4; i++) {
@@ -19,15 +19,13 @@
 
             // wrap around if at end of item collection
             if (!itemToClone.length) {
-                itemToClone = $(this).siblings(":first");
+                itemToClone = $(this).siblings(':first');
             }
 
             // grab item, clone, add marker class, add to collection
-            itemToClone
-                .children(":first-child")
-                .clone()
-                .addClass("cloneditem-" + i)
+            itemToClone.children(':first-child').clone()
+                .addClass("cloneditem-" + (i))
                 .appendTo($(this));
         }
     });
-})();
+}());
