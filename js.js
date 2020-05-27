fe4bf53d197dc4c25601685d8e7370e735a730a4
dittes-verden <sandra.birkefeldt@live.dk>
2020-05-27 22:03:39 +0200

    Version 31

--- js.js
+++ js.js
@@ -1,14 +1,5 @@
 'use strict';
 
-function myFunction() {
-    var x = document.getElementById("nav");
-    if (x.className === "nav-bar") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav-bar";
-    }
-}
-
 function dialog(hoejde, bredde, baggrundsfarve, fontfarve, placering, indhold, knapper) {
 
     const htmlkrop = document.body;
